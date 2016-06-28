3a02314800f69f2adc0330152401bd5abb8990cd
Hannah Swift <h.graceswift@gmail.com>
2016-06-28 14:50:35 -0500

    Complete select-action

--- src/blackjack/select-action.js
+++ src/blackjack/select-action.js
@@ -1,10 +1,14 @@
 import shouldHit from 'blackjack/should-hit';
 import shouldSplit from 'blackjack/should-split';
+import cardValue from 'blackjack/card-value';
+import cardSum from 'blackjack/card-sum';
 
 export default function selectAction(cardA, cardB){
-  if(shouldSplit() === true) {
+
+  if(shouldHit(cardA, cardB)) {
+    return 'hit';
+  } else if (shouldSplit(cardA, cardB)) {
     return 'split';
   }
-
-  return 'hit';
+  return 'stay';
 }
