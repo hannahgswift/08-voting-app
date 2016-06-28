038a6a04dfad38889093d491983caedc001264ae
Hannah Swift <h.graceswift@gmail.com>
2016-06-28 14:39:51 -0500

    Solve should-hit

--- src/blackjack/select-action.js
+++ src/blackjack/select-action.js
@@ -1,3 +1,10 @@
-export default function selectAction(cardA, cardB) {
+import shouldHit from 'blackjack/should-hit';
+import shouldSplit from 'blackjack/should-split';
 
+export default function selectAction(cardA, cardB){
+  if(shouldSplit() === true) {
+    return 'split';
+  }
+
+  return 'hit';
 }
