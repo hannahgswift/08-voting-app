d83457b0cf6735409a611a3a3f14e6294b3ae417
Hannah Swift <h.graceswift@gmail.com>
2016-06-28 15:38:47 -0500

    Solve calculate-percent

--- src/blackjack/select-action.js
+++ src/blackjack/select-action.js
@@ -5,7 +5,7 @@ import cardSum from 'blackjack/card-sum';
 
 export default function selectAction(cardA, cardB){
 
-  if(shouldHit(cardA, cardB)) {
+  if(shouldHit(cardA, cardB)){
     return 'hit';
   } else if (shouldSplit(cardA, cardB)) {
     return 'split';
