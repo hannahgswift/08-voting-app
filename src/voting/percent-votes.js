07ec22901a71230133966cdc3e04d95d1555a9ed
Hannah Swift <h.graceswift@gmail.com>
2016-06-29 15:29:51 -0500

    percent-votes.js

--- src/voting/percent-votes.js
+++ src/voting/percent-votes.js
@@ -1,3 +1,12 @@
-export default function percentVotes(options, index) {
+import totalVotes from 'voting/total-votes';
+import calculatePercent from 'voting/calculate-percent';
+
+
+export default function percentVotes(options, index){
+
+  let x = options[index].votes;
+  let y = totalVotes(options, index);
+
+  return calculatePercent(x, y);
 
 }
